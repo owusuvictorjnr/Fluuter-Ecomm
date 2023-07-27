6374d87f2959811efeaf35a209be3d2f89a21570
owusuvictorjnr <owusuvictor80@gmail.com>
2023-07-27 17:22:45 -0400

    Account setup

--- lib/features/auth/screens/auth_screen.dart
+++ lib/features/auth/screens/auth_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
+import '../../../constants/global_variables.dart';
+
 class AuthScreen extends StatefulWidget {
+  static const String routeName = '/auth-screen';
+
   const AuthScreen({super.key});
 
   @override
@@ -10,6 +14,27 @@ class AuthScreen extends StatefulWidget {
 class _AuthScreenState extends State<AuthScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(
+      backgroundColor: GlobalVariables.greyBackgroundColor,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Text(
+                'Welcome to Vitech Ecomm',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+              ),
+              ListTile(
+                title: Text(
+                  'Create Account',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
