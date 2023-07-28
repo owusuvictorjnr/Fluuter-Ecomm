42d4311d22e4bfbebe74c60efbb56d8a6cc3ee61
owusuvictorjnr <owusuvictor80@gmail.com>
2023-07-28 10:50:47 -0400

    create login form

--- lib/features/auth/screens/auth_screen.dart
+++ lib/features/auth/screens/auth_screen.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 
 import '../../../constants/global_variables.dart';
 
+enum Auth {
+  signin,
+  signup,
+}
+
 class AuthScreen extends StatefulWidget {
   static const String routeName = '/auth-screen';
 
@@ -12,26 +17,74 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  Auth _auth = Auth.signup;
+  final _signUpFormKey = GlobalKey<FormState>();
+  final _signInFormKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: GlobalVariables.greyBackgroundColor,
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
-              Text(
+              const Text(
                 'Welcome to Vitech Ecomm',
                 style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
               ),
               ListTile(
-                title: Text(
+                title: const Text(
                   'Create Account',
-                  style: TextStyle(fontWeight: FontWeight.bold),
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                leading: Radio(
+                  activeColor: GlobalVariables.secondaryColor,
+                  value: Auth.signup,
+                  groupValue: _auth,
+                  onChanged: (Auth? val) {
+                    setState(
+                      () {
+                        _auth = val!;
+                      },
+                    );
+                  },
+                ),
+              ),
+
+              if (_auth == Auth.signup)
+                Form(
+                  key: _signUpFormKey,
+                  child: const Column(
+                    
+                  ),
+                ),
+
+              // Sign in
+              ListTile(
+                title: const Text(
+                  'Sign in ',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                leading: Radio(
+                  activeColor: GlobalVariables.secondaryColor,
+                  value: Auth.signin,
+                  groupValue: _auth,
+                  onChanged: (Auth? val) {
+                    setState(
+                      () {
+                        _auth = val!;
+                      },
+                    );
+                  },
                 ),
               )
-            ],
+            ], // Children
           ),
         ),
       ),
