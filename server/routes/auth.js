211b53a9016fc60f081959ac4e0aeeaa1b43790d
owusuvictorjnr <owusuvictor80@gmail.com>
2023-07-31 11:13:20 -0400

    auth user

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const User = require('../models/user')
 
 const authRouter = express.Router()
 
-authRouter.post('/api/signup', (req, res) => {
+authRouter.post('/api/signup', async (req, res) => {
   const { name, email, password } = req.body
+
+  const existUser = await User.findOne({ email })
+  if (existUser) {
+    return res.status(400).json({ msg: 'Email already exists' })
+  }
+
+  let user = new User({
+    email,
+    password,
+    name,
+  })
+  user = await user.save()
 })
 
 module.exports = authRouter
