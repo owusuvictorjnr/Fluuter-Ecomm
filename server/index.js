15387dd462ac6b7bc3c7f4bcdeb822b224a9e26a
owusuvictorjnr <owusuvictor80@gmail.com>
2023-07-31 10:15:14 -0400

    middleware and db connection

--- server/index.js
+++ server/index.js
@@ -1,8 +1,24 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const authRouter = require('./routes/auth')
 
+// Init
 const PORT = 3000
-
 const app = express()
+const DB =
+  'mongodb+srv://vitech:Pinkhode@cluster0.8trahms.mongodb.net/?retryWrites=true&w=majority'
+// Middleware
+app.use(authRouter)
+
+// Coonection
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log('Connection successful')
+  })
+  .catch((err) => {
+    console.log(err)
+  })
 
 // Creating API
 app.listen(PORT, '0.0.0.0', () => {
